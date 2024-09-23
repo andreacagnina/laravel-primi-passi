bcf03aa0041e24d767020caf25feef1602846314
andreacagnina <amirkal@libero.it>
2024-09-23 15:29:47 +0200

    [UPD] DYNAMIC H1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="content text-center">
-                        <h1>Hello World</h1>
+                        <h1>{{ $greeting }}</h1>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+    $greeting = 'Hello World';
+    return view('home', compact('greeting'));
+})->name('greeting');
